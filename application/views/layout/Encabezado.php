2785d73c974a7f6da1a7a00ac5b5124e0b506b40
SE20582 <SE20582@P485197.grupo.ypf.com>
2017-10-11 20:45:15 -0300

    Se corrigen estilos Lupa de busqueda y se agrega libreria FontAwesome

--- application/views/layout/Encabezado.php
+++ application/views/layout/Encabezado.php
@@ -29,7 +29,7 @@ and open the template in the editor.
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url("assets/font-awesome/css/font-awesome.min.css");?>"/>
         <style>
             @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700i,900');
         </style> 
