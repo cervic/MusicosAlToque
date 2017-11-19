f8e856f0773b0b25fe59ea1f2ed1b1f56d79cf3b
SE20582 <SE20582@P485197.grupo.ypf.com>
2017-11-19 13:01:46 -0300

    Modificacion en Resultados, pie de pagina y estiloshome

--- application/views/ResultadosView.php
+++ application/views/ResultadosView.php
@@ -3,26 +3,70 @@
         <div id="resultados">
             <ul class="list-unstyled">
               <li class="media">
-                    <img class="mr-3" src="..." alt="Generic placeholder image">
+                    <img class="mr-3" src=".." alt="Instrumento">
                     <div class="media-body">
-                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
                       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     </div>
               </li>
               <li class="media my-4">
-                    <img class="mr-3" src="..." alt="Generic placeholder image">
+                    <img class="mr-3" src=".." alt="Instrumento">
                     <div class="media-body">
-                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
                       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     </div>
               </li>
               <li class="media">
-                    <img class="mr-3" src="..." alt="Generic placeholder image">
+                    <img class="mr-3" src=".." alt="Instrumento">
                     <div class="media-body">
-                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
                       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     </div>
               </li>
+              
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              <li class="media">
+                    <img class="mr-3" src=".." alt="Instrumento">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">Nombre de Uusuario</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+              </li>
+              
             </ul>	
         </div>
 	
--- application/views/layout/PiePagina.php
+++ application/views/layout/PiePagina.php
@@ -1,6 +1,37 @@
+               <footer>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm-4">
+                          <ul>
+                              <li>Home</li>
+                              <li>Búsqueda</li>
+                              <li>Explorar</li>
+                          </ul>
+                      </div>
+                      <div class="col-sm-4">
+                        <ul>
+                              <li>Musicios</li>
+                              <li>Bandas</li>
+                              <li>Eventos</li>
+                          </ul>
+                      </div>
+                      <div class="col-sm-4">
+                        <ul>
+                              <li>Facebook</li>
+                              <li>Twiter</li>
+                              <li>Google+</li>
+                          </ul>
+                      </div>
+                    </div>
+                  </div>
+                   
+                </footer>
             </div>
+            
         </div>
 
+     
+        
         <!-- POP UP-->
         <div class="modal fade" id="LayoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
